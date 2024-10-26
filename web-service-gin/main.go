5cf76f3917c0b96632cb0f0e82c1108ae7ef2642
abdusss111 <abdusalamabdu29@gmail.com>
2024-10-26 23:17:16 +0500

    2nd endpoint

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -1,10 +1,9 @@
 package main
 import (
     "net/http"
-
     "github.com/gin-gonic/gin"
 )
-// album represents data about a record album.
+
 type user struct {
     ID     string  `json:"id"`
     Name  string  `json:"name"`
@@ -12,11 +11,25 @@ type user struct {
     Course  int `json:"course"`
 }
 
-// users slice to seed record album data.
+type course struct {
+    Title string `json:"title"`
+    Grade float64 `json:"grade"`
+    Description string `json:"description"`
+    Prerequisites string `json: "prerequisites"`
+}
+
+// users 
 var users = []user{
     {ID: "1", Name: "Abdussalam", Age: 19, Course: 3},
     {ID: "2", Name: "Abdussalyam", Age: 19, Course: 2},
     {ID: "3", Name: "Abdusalam", Age: 19, Course: 1},
+}
+
+var courses = []course{
+    {Title: "OOP", Grade: 4.0, Description: "Object Oriented Programming using Java", Prerequisites: "PP1, PP2" },
+    {Title: "Angular", Grade: 4.0, Description: "Frontend Framework Angular TS", Prerequisites: "PP1, PP2, WD" },
+    {Title: "DSA", Grade: 4.0, Description: "Data Storage and Analysis", Prerequisites: "no prerequisites" },
+
 }
 
 // getusers responds with the list of all users as JSON.
@@ -24,9 +37,15 @@ func getUsers(c *gin.Context) {
     c.IndentedJSON(http.StatusOK, users)
 }
 
+func getCourses(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, courses)
+}
+
 func main() {
     router := gin.Default()
-    router.GET("/users", getUsers)
+    router.GET("courses", getCourses)
+
+    router.GET("users", getUsers)
 
     router.Run("localhost:8080")
 }
